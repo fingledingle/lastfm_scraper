405c245c26ad24ca3de6029e141db6ea21a3b78a
mmmmmm <elenai@riseup.net>
2024-04-06 07:22:54 -1200

    finished lastfm part on the main page

--- login.py
+++ login.py
@@ -14,64 +14,91 @@ class MainWindow:
         self.window.geometry("500x400")
         self.window.title("Spotify ya' hallo!")
 
+        #images
+        self.danbo_face_image = CTkImage(Image.open("./images/button.png"), size=(30, 20))
+        self.danbo = CTkImage(Image.open("./images/danbo.png"), size=(158, 119))
+        self.yotsuba_logo = CTkImage(Image.open("./images/yotsuba_png.png"), size=(100, 100))
+        self.yotsuba_image = CTkImage(Image.open("./images/yotsuba.png"), size=(150, 150))
+        self.spotify_image = CTkImage(Image.open("./images/spotify.png"), size=(30, 30))
+        self.scrapingbee_image = CTkImage(Image.open("./images/bee.png"), size=(30, 30))
 
     def program_windows(self):
 
-        def button(spotify, scrapingbee, key_frame):
+        def send_keys_button(spotify, scrapingbee, key_frame):
             key_frame.destroy()
             main_page()
+            print(scrapingbee)
             print(spotify)
+
+        def start_search_button(artist_name):
+            pass
 
 
         def lastfm_button():
 
+            self.menu_frame = CTkFrame(self.window, height=390, width=352, corner_radius=25)
+            self.menu_frame.place(x=146, y=5)
+
             def same_genre():
+                pass
+
+            def result():
                 pass
 
 
 
 
 
-
-            self.menu_frame = CTkFrame(self.window, height=390, width=352, corner_radius=25)
-            self.menu_frame.place(x=146, y=5)
-
             def similar_artists():
-                self.similar_artist = customtkinter.CTkEntry(master=self.menu_frame, placeholder_text="", width=150,
-                                                            corner_radius=50)
-                self.similar_artist.place(x=101, y=270)
+                self.similar_artist_frame = CTkFrame(self.menu_frame, height=130, width=230, corner_radius=50)
+                self.similar_artist_frame.place(x=62, y=245)
+
+
+                self.similar_artist = CTkEntry(master=self.similar_artist_frame, placeholder_text="artist name", width=150, corner_radius=50)
+                self.similar_artist.place(x=42, y=20)
+
+
+
+                login_button = customtkinter.CTkButton(self.similar_artist_frame, text="", image=self.danbo_face_image,
+                                                       fg_color="transparent",
+                                                       border_color="white", hover_color="grey", border_width=1,
+                                                       corner_radius=5,
+                                                       height=40, width=60, command=lambda: start_search_button(self.similar_artist.get()
+                                                                                                   )
+                                                       )
+                login_button.place(x=85, y=60)
+
 
             similar_artists()
 
 
 
-            self.danbo = CTkImage(Image.open("./images/danbo.png"), size=(158, 119))
+
             self.yotsuba = customtkinter.CTkLabel(self.menu_frame, text="", fg_color="transparent",
                                              image=self.danbo)
-            self.yotsuba.place(x=105, y=55)
+            self.yotsuba.place(x=105, y=25)
 
 
 
 
             self.methods = customtkinter.CTkSegmentedButton(master=self.menu_frame, values=["similar artists", "same genre"], width=160)
-            self.methods.place(x=96, y=180)
+            self.methods.place(x=96, y=150)
             self.methods.set("methods")
 
 
 
-            self.artist_label = customtkinter.CTkLabel(master=self.menu_frame, text="QT.Artist", fg_color="transparent")
-            self.artist_label.place(x=120, y=220)
+            self.artist_label = customtkinter.CTkLabel(master=self.menu_frame, text="QT.Artists", fg_color="transparent")
+            self.artist_label.place(x=115, y=180)
             self.artist_quantity = customtkinter.CTkEntry(master=self.menu_frame, placeholder_text="", width=45, corner_radius=50)
-            self.artist_quantity.place(x=125, y=245)
+            self.artist_quantity.place(x=120, y=205)
 
 
 
 
             self.song_label = customtkinter.CTkLabel(master=self.menu_frame, text="QT.Songs", fg_color="transparent")
-            self.song_label.place(x=200, y=220)
-            self.song_quantity = customtkinter.CTkEntry(master=self.menu_frame, placeholder_text="", width=45,
-                                                          corner_radius=50)
-            self.song_quantity.place(x=205, y=245)
+            self.song_label.place(x=190, y=180)
+            self.song_quantity = customtkinter.CTkEntry(master=self.menu_frame, placeholder_text="", width=45, corner_radius=50)
+            self.song_quantity.place(x=195, y=205)
 
 
 
@@ -105,7 +132,7 @@ class MainWindow:
 
 
 
-            self.yotsuba_logo = CTkImage(Image.open("./images/yotsuba_png.png"), size=(100, 100))
+
             yotsuba = customtkinter.CTkLabel(self.menu_frame, text="", fg_color="transparent",
                                              image=self.yotsuba_logo)
             yotsuba.place(x=25, y=20)
@@ -157,7 +184,7 @@ class MainWindow:
 
 
             #Yotsuba LOL
-            self.yotsuba_image = CTkImage(Image.open("./images/yotsuba.png"), size=(150, 150))
+
             yotsuba = customtkinter.CTkLabel(self.key_frame, text="", fg_color="transparent",
                                                        image=self.yotsuba_image)
             yotsuba.place(x=175, y=40)
@@ -166,8 +193,6 @@ class MainWindow:
 
 
             #SCRAPINGBEE ENTRY AND LOGO
-
-            self.scrapingbee_image = CTkImage(Image.open("./images/bee.png"), size=(30, 30))
             scrapingbee_label = customtkinter.CTkLabel(self.key_frame, text="", fg_color="transparent", image=self.scrapingbee_image)
             scrapingbee_label.place(x=70, y=193)
 
@@ -179,8 +204,6 @@ class MainWindow:
 
 
             #SPOTIFY ENTRY AND LOGO
-
-            self.spotify_image = CTkImage(Image.open("./images/spotify.png"), size=(30, 30))
             spotify_label = customtkinter.CTkLabel(self.key_frame, text="", fg_color="transparent", image=self.spotify_image)
             spotify_label.place(x=70, y=245)
 
@@ -195,11 +218,10 @@ class MainWindow:
 
 
             #Button
-            self.danbo_face_image = CTkImage(Image.open("./images/button.png"), size=(30, 20))
 
             login_button = customtkinter.CTkButton(self.key_frame, text="", image=self.danbo_face_image, fg_color="transparent",
                                                    border_color="white", hover_color="grey", border_width=1, corner_radius=5,
-                                                   height=40, width=60, command=lambda: button(self.spotify.get(), self.scrapingbee.get(), self.key_frame))
+                                                   height=40, width=60, command=lambda: send_keys_button(self.spotify.get(), self.scrapingbee.get(), self.key_frame))
             login_button.place(x=220, y=300)
 
 
