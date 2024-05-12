ace1b8ac692a166f5290ed584a183e8fe669b9c3
fingledingle <elenai@riseup.net>
2024-05-13 03:05:24 +0900

    finished spotify just making it oop to link wityh the ui

--- spotify.py
+++ spotify.py
@@ -4,23 +4,82 @@
 #http://localhost:3000.
 
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
 # Set up Spotipy with your credentials
 client_id = '2c6d4e17556345be8a08e0e016de4f06'
 client_secret = '0875d4f8e27c4a088fb8e3e70145091c'
+url = 'http://example.com'
 
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-le_empty_list = []
+
+
+
+
+scope = 'playlist-modify-public playlist-modify-private'
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                               client_secret=client_secret,
+                                               redirect_uri='http://example.com',
+                                               show_dialog=True,
+                                               scope=scope
+                                               )
+                    )
+
+
+
+
+
+
+
 
 def get_most_played_song_uri(artist_name):
-
-    for artists in artist_name:
+    song_list = []
+    for artists in artist_name:    
         result = sp.search(q=artists, type='artist')
-        le_empty_list.append(result)
-    return result['artists']['items'][0]['id']
+        if len(result['artists']['items']) > 0:
+            artists_id = result['artists']['items'][0]['id']
+            top_tracks = sp.artist_top_tracks(artists_id)
+            num_tracks_to_return = min(2,len(top_tracks['tracks']))
+            print(f'Top {num_tracks_to_return} tracks for {artists}:')
+            for track in top_tracks['tracks'][:num_tracks_to_return]:
+                print(f'adding the song {track["name"]} by {artists} to playlist')
+                song_list.append(track['uri'])
+        else:
+            print(f'No artist was found with the name "{artists}". ')
+    return song_list
+
+
+
+
+
+
+user_id = sp.me()['id']
+
+playlist_name = 'Hello my name is jeff'
+
+playlist = sp.user_playlist_create(user_id, playlist_name, public=False)
+
+playlist_id = playlist['id']
+
+
+
+artist_name = ['bladee', 'thaiboy', 'ecco2k']
+jeff = get_most_played_song_uri(artist_name)
+
+
+
+
+
+sp.user_playlist_add_tracks(user_id, playlist_id, jeff)
+
+
+
+
+
+
+
+
+
 
 
     # artist_id = result['artists']['items'][0]['id']
@@ -37,11 +96,11 @@ def get_most_played_song_uri(artist_name):
 
 
 
-artist_name = ('bladee, thaiboy, ecco2k')
+# artist_name = ['bladee', 'thaiboy', 'ecco2k']
 
 
-most_played_song = get_most_played_song_uri(artist_name)
-print(f'try to find the artists and it returns{most_played_song}')
+# most_played_song = get_most_played_song_uri(artist_name)
+# print(f'try to find the artists and it returns{le_empty_list}')
 
 
 # print(f'the most played song by {artist_name} is {most_played_song}')
--- test.py
+++ test.py
@@ -1,10 +1,46 @@
-cum = 'poo.poo1.poo2'
-new_cum = []
-for dot in cum:
-    if dot == '.':
-        dot = ' '
-    new_cum.append(dot)
-new_cum = ''.join(new_cum)
-print(new_cum)
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Setup credentials and Spotify client
+client_id = '2c6d4e17556345be8a08e0e016de4f06'
+client_secret = '0875d4f8e27c4a088fb8e3e70145091c'
+redirect_uri = 'http://example.com'
+scope = 'playlist-modify-public playlist-modify-private'
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                               client_secret=client_secret,
+                                               redirect_uri=redirect_uri,
+                                               scope=scope))
+
+def get_most_played_song_uri(artist_name):
+    song_uris = []  # List to store song URIs
+    for artist in artist_name:
+        result = sp.search(q=artist, type='artist')
+        if result['artists']['items']:
+            artist_id = result['artists']['items'][0]['id']
+            top_tracks = sp.artist_top_tracks(artist_id)
+            for track in top_tracks['tracks'][:2]:  # Get top 2 tracks
+                song_uris.append(track['uri'])
+                print(f'Adding {track["name"]} by {artist}')
+    return song_uris
+
+# Correctly set your playlist_id to a valid Spotify playlist URI
 
 
+user_id = sp.me()['id']
+
+playlist_name = 'Hello my name is jeff'
+
+playlist = sp.user_playlist_create(user_id, playlist_name, public=False)
+    
+playlist_id = playlist['id']
+
+
+
+artist_names = ['bladee', 'thaiboy', 'ecco2k']
+track_uris = get_most_played_song_uri(artist_names)
+
+# Add tracks to playlist and change playlist details
+if track_uris:
+    sp.playlist_add_items(playlist_id, track_uris)
+else:
+    print("No tracks to add.")
