4dbeb7b8ccf50bd5b4d5a19c9f328880cd61db6c
fingledingle <elenai@riseup.net>
2024-05-13 03:58:35 +0900

    well le fail

--- spotify.py
+++ spotify.py
@@ -1,7 +1,4 @@
 
-# 2c6d4e17556345be8a08e0e016de4f06
-# 0875d4f8e27c4a088fb8e3e70145091c
-#http://localhost:3000.
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -74,62 +71,15 @@ class Spotify_thingy:
 
 
 artists_names = ['uami', 'arakoi', 'nuito']
-quantity = 3
-client_id = '2c6d4e17556345be8a08e0e016de4f06'
-client_secret = '0875d4f8e27c4a088fb8e3e70145091c'
 
+quantity = 3
+
+client_id = '2722e7254a20426488a969a2b94cdfff'
+client_secret = '9a65f1340a5441e6a1de69a837043796'
 
 
 spotify = Spotify_thingy(artists_names=artists_names, song_quantity=quantity, client_id=client_id, client_secret=client_secret)
 start_searching = spotify.search_and_add()
-
-
-
-def get_most_played_song_uri(artist_name):
-    song_list = []
-    for artists in artist_name:    
-        result = sp.search(q=artists, type='artist')
-        if len(result['artists']['items']) > 0:
-            artists_id = result['artists']['items'][0]['id']
-            top_tracks = sp.artist_top_tracks(artists_id)
-            num_tracks_to_return = min(2,len(top_tracks['tracks']))
-            print(f'Top {num_tracks_to_return} tracks for {artists}:')
-            for track in top_tracks['tracks'][:num_tracks_to_return]:
-                print(f'adding the song {track["name"]} by {artists} to playlist')
-                song_list.append(track['uri'])
-        else:
-            print(f'No artist was found with the name "{artists}". ')
-    return song_list
-
-
-
-
-
-
-# user_id = sp.me()['id']
-
-# playlist_name = 'Hello my name is jeff'
-
-# playlist = sp.user_playlist_create(user_id, playlist_name, public=False)
-
-# playlist_id = playlist['id']
-
-
-
-# artist_name = ['bladee', 'thaiboy', 'ecco2k']
-# jeff = get_most_played_song_uri(artist_name)
-
-
-
-
-
-# sp.user_playlist_add_tracks(user_id, playlist_id, jeff)
-
-
-
-
-
-
 
 
 
